d3ba3dd19aa9c75461533aa6354193504abe6ea8
Pituivan <ivangranada00@gmail.com>
2025-02-23 20:49:59 +0100

    Attempted to add Mac and Linux support. Haven't tested it yet

--- Scripts/PlayerPrefsReader.cs
+++ Scripts/PlayerPrefsReader.cs
@@ -1,10 +1,16 @@
-using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using UnityEngine;
+
+#if UNITY_EDITOR_WIN
+using Microsoft.Win32;
+using System.Text;
+#elif UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
+using System.IO;
+using System.Xml.Linq;
+#endif
 
 namespace Pituivan.EditorTools.PlayerPrefsManager
 {
@@ -20,18 +26,46 @@ namespace Pituivan.EditorTools.PlayerPrefsManager
             "UnityGraphicsQuality"
         };
 
+#if UNITY_EDITOR_WIN
         private static readonly string registryPath;
+#elif UNITY_EDITOR_OSX
+    private static readonly string plistPath;
+#elif UNITY_EDITOR_LINUX
+    private static readonly string playerPrefsPath;
+#endif
 
         // ----- Constructors
 
         static PlayerPrefsReader()
         {
+#if UNITY_EDITOR_WIN
             registryPath = $@"Software\Unity\UnityEditor\{Application.companyName}\{Application.productName}";
+#elif UNITY_EDITOR_OSX
+            plistPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                $"Library/Preferences/unity.{Application.companyName}.{Application.productName}.plist");
+#elif UNITY_EDITOR_LINUX
+            playerPrefsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                $".config/unity3d/{Application.companyName}/{Application.productName}/prefs");
+#endif
+        }
+
+        // ----- Public Methods
+
+        public static IList<PlayerPref> ListPlayerPrefs()
+        {
+#if UNITY_EDITOR_WIN
+            return ListPlayerPrefsWin();
+#elif UNITY_EDITOR_OSX
+            return ListPlayerPrefsMac();
+#elif UNITY_EDITOR_LINUX
+            return ListPlayerPrefsLinux();
+#endif
         }
 
         // ----- Private Methods
 
-        public static IList<PlayerPref> ListPlayerPrefs()
+#if UNITY_EDITOR_WIN
+        private static IList<PlayerPref> ListPlayerPrefsWin()
         {
             using RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(registryPath) 
                 ?? throw new InvalidOperationException("Couldn't access Player Prefs keys in registry.");
@@ -57,5 +91,86 @@ namespace Pituivan.EditorTools.PlayerPrefsManager
 
             return playerPrefs;
         }
+#endif
+
+#if UNITY_EDITOR_OSX
+        private static IList<PlayerPref> ListPlayerPrefsMac()
+        {
+            XDocument plist = XDocument.Load(plistPath) ?? throw new InvalidOperationException("Couldn't access .plist file.");
+            XElement dict = plist.Descendants("dict").First() ?? throw new InvalidOperationException("Invalid .plist file.");
+
+            var playerPrefs = new List<PlayerPref>();
+
+            List<XElement> elements = dict.Elements("key").ToList();
+            for (int i = 0; i < elements.Count - 1; i += 2)
+            {
+                XElement keyElement = elements[i];
+                string key = keyElement.Value;
+
+                if (defaultPlayerPrefs.Contains(key)) continue;
+
+                var valueElement = keyElement.NextNode as XElement;
+                object value = valueElement.Name.LocalName switch
+                {
+                    "integer" => int.Parse(valueElement.Value),
+                    "real" => float.Parse(valueElement.Value, System.Globalization.CultureInfo.InvariantCulture),
+                    "string" => valueElement.Value,
+                    _ => throw new InvalidOperationException("Unexpected Player Pref type.")
+                };
+
+                playerPrefs.Add(new PlayerPref(key, value));
+            }
+
+            return playerPrefs;
+        }
+#endif
+
+#if UNITY_EDITOR_LINUX
+        private static IList<PlayerPref> ListPlayerPrefsLinux()
+        {
+            byte[] playerPrefsData = File.ReadAllBytes(playerPrefsPath);
+
+            var playerPrefs = new List<PlayerPref>();
+            int i = 0;
+            while (i < playerPrefsData.Length)
+            {
+                int keyLength = BitConverter.ToInt32(playerPrefsData, i);
+                i += 4;
+                string key = Encoding.UTF8.GetString(playerPrefsData, i, keyLength);
+                i += keyLength;
+
+                byte type = playerPrefsData[i];
+                i++;
+
+                object value;
+                switch (type)
+                {
+                    case 0: // Int
+                        value = BitConverter.ToInt32(playerPrefsData, i);
+                        i += 4;
+                        break;
+
+                    case 1: // Float
+                        value = BitConverter.ToSingle(playerPrefsData, i);
+                        i += 4;
+                        break;
+
+                    case 2: // String
+                        int length = BitConverter.ToInt32(playerPrefsData, i);
+                        i += 4;
+                        value = Encoding.UTF8.GetString(playerPrefsData, i, length);
+                        i += length;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"Unexpected Player Pref type.");
+                }
+
+                playerPrefs.Add(new PlayerPref(key, value));
+            }
+
+            return playerPrefs;
+        }
+#endif
     }
 }
